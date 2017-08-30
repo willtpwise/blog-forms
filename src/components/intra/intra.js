40dc86eba293b7d2bea64e84f900315de02cb93d
William Wise <williamwise@Will-Wise.local>
2017-08-30 16:51:04 +1000

    Hiding the form when the user has already signed up

--- src/components/intra/intra.js
+++ src/components/intra/intra.js
@@ -1,1 +1,26 @@
 import './intra.scss'
+
+class BlogIntraForm {
+  constructor (target) {
+    this.el = target
+    let form = this.el.querySelector('form')
+
+    form.addEventListener('prefilled', (e) => {
+      let lead = e.detail
+      if (lead.SM_Blog__c) {
+        this.hide()
+      }
+    })
+  }
+
+  hide () {
+    this.el.className += ' intra-form--disabled'
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector('.intra-form')
+  if (form) {
+    return new BlogIntraForm(form)
+  }
+})
